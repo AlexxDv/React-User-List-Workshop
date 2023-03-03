79d928e584484106201a166f8fdf720e1b03665f
AlexxDv <alexxdimov@gmail.com>
2023-03-03 15:55:13 +0100

    уп

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import { Fragment } from "react"
 
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import "./App.css"
+
 
 function App() {
   return (
-    <Fragment>
+    <>
 
       < Header />
+
+      <main className="main">
+        < Search />
+
+      </main>
+
       < Footer />
 
-    </Fragment>
+    </>
   );
 }
 
