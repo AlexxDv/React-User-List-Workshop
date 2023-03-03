dc2b888b220184208d78bb0724c467e9fd189172
AlexxDv <alexxdimov@gmail.com>
2023-03-03 16:39:46 +0100

    up

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
+import { UserList } from "./components/UserList";
 import "./App.css"
 
 
@@ -13,6 +14,7 @@ function App() {
 
       <main className="main">
         < Search />
+        < UserList />
 
       </main>
 
