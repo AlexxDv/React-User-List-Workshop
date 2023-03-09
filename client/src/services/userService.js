ca4724fa695cb34bcafbe53ae0a5c71df9359439
AlexxDv <alexxdimov@gmail.com>
2023-03-09 12:16:09 +0100

    Update userService.js

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,6 +3,7 @@ const baseUrl = "http://localhost:3005/api/users"
 
 export const getAll = async () => {
     const responce = await fetch(baseUrl)
+    
     const result = await responce.json()
 
     return result.users;
